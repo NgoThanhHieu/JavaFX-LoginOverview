187ececbbc0dbe08c78383f4b03dbfb16f455115
MilanNgo012 <66179085+MilanNgo012@users.noreply.github.com>
2024-08-21 15:55:49 +0200

    testing_commit

--- ProjectLoginOverview/src/main/java/com/mycompany/projectloginoverview/Register_viewController.java
+++ ProjectLoginOverview/src/main/java/com/mycompany/projectloginoverview/Register_viewController.java
@@ -19,7 +19,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
